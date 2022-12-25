c05d0ee90a93aa51ace96b30757e0d17a6bf81a1
im-suryansh <tsuryansh53@gmail.com>
2022-12-25 16:28:10 +0530

    background contrast

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ function animate(){
     c.fillRect(0,0,canvas.width,canvas.height)
     background.update()
     shop.update()
-    c.fillStyle = 'rgba(255, 255, 255, 0.15)'
+    c.fillStyle = 'rgba(255, 255, 255, 0.01)'
   c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
